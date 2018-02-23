113123b19d43484047c75fc9315e3c29bce40d6e
Will Prater <will@superformula.com>
2018-02-22 19:45:58 -0800

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
-# function fish_prompt --description 'Write out the prompt'
+function xx_fish_prompt --description 'Write out the prompt'
 
   # Use after set_color to reset bold
   if not set -q __fish_prompt_color_normal
@@ -43,6 +43,79 @@
   end
 
   echo -n -s "$__fish_prompt_color_hostname" "$__fish_prompt_hostname" "$__fish_prompt_color_normal" ':' "$__fish_prompt_color_cwd" (prompt_pwd) "$__fish_prompt_color_git" (__fish_git_prompt "#%s") "$__fish_prompt_color_normal" '$ '
+
+end
+
+# name: clearance
+# ---------------
+# Based on idan. Display the following bits on the left:
+# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
+# - Current directory name
+# - Git branch and dirty state (if inside a git repo)
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty --untracked-files=no ^/dev/null)
+end
+
+function fish_prompt
+  set -l last_status $status
+
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l blue (set_color blue)
+  set -l green (set_color green)
+  set -l normal (set_color normal)
+
+  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
+
+  # If commands takes longer than 10 seconds, notify user on completion if Terminal
+  # in background. (Otherwise e.g. reading man pages for longer than 10 seconds will
+  # trigger the notification.) Inspired by https://github.com/jml/undistract-me/issues/32.
+  if test $CMD_DURATION
+    if test $CMD_DURATION -gt (math "1000 * 10")
+      if not terminal-frontmost
+        set secs (math "$CMD_DURATION / 1000")
+        # It's not possible to raise the window via the notification; see
+        # https://stackoverflow.com/a/33808356
+        notify "$history[1]" "(status $status; $secs secs)"
+      end
+    end
+  end
+
+  # Output the prompt, left to right
+
+  # Add a newline before new prompts
+  echo -e ''
+
+  # Display [venvname] if in a virtualenv
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
+  end
+
+  # Print pwd or full path
+  echo -n -s $cwd $normal
+
+  # Show git branch and status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '(' $yellow $git_branch "±" $normal ')'
+    else
+      set git_info '(' $green $git_branch $normal ')'
+    end
+    echo -n -s ' · ' $git_info $normal
+  end
+
+  set -l prompt_color $red
+  if test $last_status = 0
+    set prompt_color $normal
+  end
 
   # Terminate with a nice prompt char
   echo -e ''
